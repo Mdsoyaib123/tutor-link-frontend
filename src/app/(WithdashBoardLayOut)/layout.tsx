017f2e1b382b7217c01950dc89d733b1d6e7f5c4
souparnaPersonal <souparna555@gmail.com>
2025-04-22 21:19:12 +0530

    studnet dashboard addeded responsive

--- src/app/(WithdashBoardLayOut)/layout.tsx
+++ src/app/(WithdashBoardLayOut)/layout.tsx
@@ -3,9 +3,9 @@ import React from "react";
 
 const Dashboardlayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="flex">
+    <div className="flex-col sm:flex-row md:flex">
       <Sidebar />
-      <div className="w-[80%]"> {children}</div>
+      <div className="w-[80%] mt-20 mx-auto sm:mt-20 md:mt-20"> {children}</div>
     </div>
   );
 };
--- src/components/dashboard/student/Profile.tsx
+++ src/components/dashboard/student/Profile.tsx
@@ -23,7 +23,7 @@ export default function ProfileCard() {
   };
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+    <div className="flex items-center justify-center ">
       {!editMode ? (
         <div className="bg-white shadow-lg rounded-xl p-6 w-[350px] text-center">
           <img
